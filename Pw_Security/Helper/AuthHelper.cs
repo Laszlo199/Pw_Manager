3084451e7baae06b4538e959773c243936176d9a
soma199 <70133057+Laszlo199@users.noreply.github.com>
2023-10-06 13:58:45 +0200

    add toka

--- Pw_Security/Helper/AuthHelper.cs
+++ Pw_Security/Helper/AuthHelper.cs
@@ -1,6 +1,14 @@
 ﻿namespace Pw_Security.Helper;
 
-public class AuthHelper
+public class AuthHelper: IAuthHelper
 {
-    
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Pw_Security/Helper/IAuthHelper.cs
+++ Pw_Security/Helper/IAuthHelper.cs
@@ -2,5 +2,7 @@
 
 public interface IAuthHelper
 {
-    
+    void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+      
+    bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
 }
