cb3eb188c7feef8ed69d90bedc8eee1c4774faa7
soma199 <70133057+Laszlo199@users.noreply.github.com>
2023-10-08 17:17:41 +0200

    Update PwManagerContext.cs

--- Pw_Manager/Db/PwManagerContext.cs
+++ Pw_Manager/Db/PwManagerContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Pw_Manager.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using Pw_Manager.Model;
+
+namespace Pw_Manager.Db;
 
 public class PwManagerContext
 {
-    
+    public PwManagerContext(DbContextOptions contextOptions) : base(contextOptions)
+    {
+    }
+
+    public DbSet<PasswordsModel> Passwords { get; set; }
 }
