608dabaebe520f42b369fe1a0560d5af394be154
Tomáš Peniak <71640446+pennyb0i@users.noreply.github.com>
2023-10-22 00:58:21 +0200

    storing masterkey in memory

--- PW_Frontend/Application/Services/AuthenticationService.cs
+++ PW_Frontend/Application/Services/AuthenticationService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using PW_Frontend.Application.Dtos.AuthDto;
@@ -9,7 +10,7 @@ namespace Pw_Frontend.Application.Services;
 public interface IAuthenticationService
 {
     TokenDto? TokenDto { get; }
-    byte[]? StretchedKey { get; }
+    byte[]? MasterKey { get; }
     Task Initialize();
     Task<bool> Register(LoginDto loginDto);
     Task<bool> Login(LoginDto loginDto);
@@ -22,7 +23,7 @@ public class AuthenticationService : IAuthenticationService
     private NavigationManager _navigationManager;
     private ILocalStorageService _localStorageService;
     public TokenDto? TokenDto { get; private set; }
-    public byte[]? StretchedKey { get; private set; }
+    public byte[]? MasterKey { get; private set; }
 
     public AuthenticationService(
         IHttpService httpService,
@@ -38,32 +39,27 @@ public class AuthenticationService : IAuthenticationService
     public async Task Initialize()
     {
         TokenDto = await _localStorageService.GetItemAsync<TokenDto>("tokenDto");
-        StretchedKey = await _localStorageService.GetItemAsync<byte[]>("stretchedKey");
     }
 
     public async Task<bool> Register(LoginDto loginDto)
     {
-        
-        
-        byte[] masterKey = EncryptionHelper.GenerateMasterKey(loginDto.Email, loginDto.Password);
-        var masterHash = EncryptionHelper.GenerateMasterHash(loginDto.Password, masterKey);
+        MasterKey = EncryptionHelper.GenerateMasterKey(loginDto.Email, loginDto.Password);
+        var masterHash = EncryptionHelper.GenerateMasterHash(loginDto.Password, MasterKey);
         var loginDtoCopy = new LoginDto {
             Email = loginDto.Email,
-            Password = System.Text.Encoding.UTF8.GetString(masterHash)
+            Password = Encoding.UTF8.GetString(masterHash)
         };
         TokenDto = await _httpService.Post<TokenDto>("/api/Auth/Register", loginDtoCopy);
         if (TokenDto is null) {
             return false;
         }
-        await _localStorageService.SetItemAsync("masterKey", masterKey);
         await _localStorageService.SetItemAsync("tokenDto", TokenDto);
         return true;
     }
     
     public async Task<bool> Login(LoginDto loginDto) {
-        
-        byte[] masterKey = EncryptionHelper.GenerateMasterKey(loginDto.Email, loginDto.Password);
-        var masterHash = EncryptionHelper.GenerateMasterHash(loginDto.Password, masterKey);
+        MasterKey = EncryptionHelper.GenerateMasterKey(loginDto.Email, loginDto.Password);
+        var masterHash = EncryptionHelper.GenerateMasterHash(loginDto.Password, MasterKey);
         var loginDtoCopy = new LoginDto {
             Email = loginDto.Email,
             Password = System.Text.Encoding.UTF8.GetString(masterHash)
@@ -72,7 +68,6 @@ public class AuthenticationService : IAuthenticationService
         if (TokenDto is null) {
             return false;
         }
-        await _localStorageService.SetItemAsync("masterKey", masterKey);
         await _localStorageService.SetItemAsync("tokenDto", TokenDto);
         return true;
     }
@@ -81,7 +76,6 @@ public class AuthenticationService : IAuthenticationService
     {
         TokenDto = null;
         await _localStorageService.RemoveItemAsync("tokenDto");
-        await _localStorageService.RemoveItemAsync("masterKey");
         _navigationManager.NavigateTo("login");
     }
 }
